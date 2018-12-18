3bd969c87d3acc095b8fdbf0849174c9ba7e998c
Jonathan <jonathankenton@gmail.com>
2018-12-18 23:29:19 +0200

    the fun stuff: data manipulation from logical structure to flat options list

--- src/components/inputSection/index.js
+++ src/components/inputSection/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import inputSection from '../../hocs/inputSection';
+import RideItem from "../rideItem";
 // import Slide from './../slide';
 
 const InputSection = ({ ...props }) => {
@@ -9,18 +10,68 @@ const InputSection = ({ ...props }) => {
     props.onChange(newInput);
   };
 
+  const options = {
+    Diamond: [
+      {
+        property: "shape",
+        value: "Emerald",
+      }, {
+        property: "color",
+        value: "E",
+      },
+    ],
+    Sapphire: [{
+      property: "shape",
+      value: "Emerald",
+    }],
+    Ruby: [{
+      property: "shape",
+      value: "Emerald",
+    }],
+  };
+
+  let listOptions = Object.entries(options).map(entry => {
+    const optionKey = entry[0];
+    const optionValue = entry[1];
+    const stoneOptions = optionValue.map(optionSingle => {
+      return [`${optionSingle.property}: ${optionSingle.value} in ${optionKey}`];
+    });
+    return stoneOptions;
+  });
+
+  let optionsFlat = []; // we need to flatten the nested arrays to single depth
+  if (typeof [].flat === "function"){ // use bleeding edge array method if available ;)
+    optionsFlat = listOptions.flat();
+  } else {
+    optionsFlat = listOptions.reduce((acc, val) => acc.concat(val), []); // revert to a conservative approach :)
+  }
+
+  const renderOptionsElements = () => {
+    return optionsFlat.map(stoneOptions => {
+      return (
+        <option value={stoneOptions[0]}/>
+      )
+    });
+  };
+
   return (
     <div className="row" id="inputSection">
       <form className="form-inline">
         <div className="form-group">
-          <input
+          {/*<input
             type="text"
             value={props.input || ""}
             onChange={this.inputChangeHandler}
             className="form-control"
             id="inputInput"
             placeholder="#INPUT"
-          />
+          />*/}
+
+          <input list="browsers" name="browser" />
+          <datalist id="browsers">
+            { renderOptionsElements() }
+          </datalist>
+
         </div>
       </form>
     </div>
