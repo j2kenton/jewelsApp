8c8faeb14bdfb78f0008f00629dc356fa179966a
Jonathan <jonathankenton@gmail.com>
2018-12-20 00:23:19 +0200

    trivial

--- src/hocs/optionItem/index.js
+++ src/hocs/optionItem/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export default function carousel(WrappedComponent) {
+export default function appComponent(WrappedComponent) {
   return class extends Component {
 
     shouldComponentUpdate(nextProps) {
